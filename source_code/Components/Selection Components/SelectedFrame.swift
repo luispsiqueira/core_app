f3ec5b0ce9a69aa76e1108a3cd4abaa28c32502b
Clissia Bovi <bovibc@gmail.com>
2024-03-25 10:13:45 -0300

    creating popup and seection field

---
+++ source_code/Components/Selection Components/SelectedFrame.swift
@@ -0,0 +1,18 @@
+//
+//  SelectedFrame.swift
+//  core_app
+//
+//  Created by Clissia Bozzer Bovi on 25/03/24.
+//
+
+import SwiftUI
+
+struct SelectedFrame: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SelectedFrame()
+}
