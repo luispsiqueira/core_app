8196f34b0a7d52b9c5358cf5404f49c28dffbce1
caio melloni <caiomelloni.contato@gmail.com>
2024-03-25 08:27:58 -0300

    adds content view

---
+++ source_code/CoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreApp.swift
+//  teste
+//
+//  Created by Caio Melloni dos Santos on 25/03/24.
+//
+
+import SwiftUI
+
+@main
+struct CoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
