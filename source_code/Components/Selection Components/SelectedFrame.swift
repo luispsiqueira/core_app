0265f7fadb51ddeeb8d7f6acd9b6a46c77b9a70c
Clissia Bovi <bovibc@gmail.com>
2024-03-26 08:40:42 -0300

    selection frame without scroll

--- source_code/Components/Selection Components/SelectedFrame.swift
+++ source_code/Components/Selection Components/SelectedFrame.swift
@@ -10,24 +10,36 @@ import SwiftUI
 struct SelectedFrame: View {
     var buttonText = "Adicionar sintoma"
     var titleText = "Seus sintomas de hoje"
+    var action: () -> Void = {}
 
     var body: some View {
         VStack {
             Text(titleText)
                 .foregroundStyle(.white)
-            ScrollView([.horizontal], showsIndicators: true) {
+                .padding(.top, 16)
+
+            ScrollView([.horizontal], showsIndicators: false) {
                 HStack(spacing: 12) {
-                    ForEach(0 ..< 10) { _ in
+                    ForEach(0 ..< 150) { _ in
                         Circle()
                             .frame(width: 60, height: 60)
                     }
                 }
             }
+            .padding(.top, 16)
+            .padding(.leading)
+            .frame(maxHeight: 50)
 
+            Divider()
+                .padding(.top, 20)
+                .padding(.bottom, -10)
+                .foregroundColor(.white)
             Button(buttonText) {
-                print("aa")
+                action()
             }
             .buttonStyle(.plain)
+            .foregroundColor(.blue)
+            .padding(.bottom, 0)
             .frame(width: 380, height: 40)
         }
         .frame(width: 380, height: 152)
