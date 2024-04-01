33848df03459dc870558e666195644dbe2f8bab7
caio melloni <caiomelloni.contato@gmail.com>
2024-04-01 11:21:15 -0300

    adds backend package

--- source_code/ContentView.swift
+++ source_code/ContentView.swift
@@ -5,20 +5,37 @@
 //  Created by Caio Melloni dos Santos on 25/03/24.
 //
 
+import BackendLib
+import SwiftData
 import SwiftUI
 
 struct ContentView: View {
+    @State private var cycleService: CycleService
+
+    init(context: ModelContext) {
+        let cycleService = CycleService(context: context)
+        _cycleService = State(initialValue: cycleService)
+    }
+
     var body: some View {
         VStack {
+            Button("create cycle") {
+                let cycle = cycleService.createCycle()
+                cycle.sympthoms?.append(CycleSymptom(day: Date(), symptom: .acne))
+            }
+            Button("fetch sympthom") {}
+            Button("add sympthom") {}
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            List {
+                ForEach(cycleService.cycles) { cycle in
+                    ForEach(cycle.sympthoms ?? []) { sym in
+                        Text(sym.symptom == .acne ? "acne" : "another one")
+                    }
+                }
+            }
         }
         .padding()
     }
-}
-
-#Preview {
-    ContentView()
 }
--- source_code/CoreApp.swift
+++ source_code/CoreApp.swift
@@ -5,13 +5,37 @@
 //  Created by Caio Melloni dos Santos on 25/03/24.
 //
 
+import BackendLib
+import SwiftData
 import SwiftUI
 
 @main
 struct CoreApp: App {
+    let container: ModelContainer
+
+    init() {
+        do {
+            container = try ModelContainer(
+                for:
+                User.self,
+                UsesMedication.self,
+                Medication.self,
+                Cycle.self,
+                CycleMood.self,
+                Period.self,
+                CycleSymptom.self
+            )
+
+        } catch {
+            fatalError("Failed to create ModelContainer")
+        }
+    }
+
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            BackendProvider {
+                ContentView(context: container.mainContext)
+            }.modelContainer(container)
         }
     }
 }
