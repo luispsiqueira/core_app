a84e8ed8a11d458fb04baf4e2e1d9cefb6ec0166
Luis Paulo Siqueira <107970831+luispsiqueira@users.noreply.github.com>
2024-04-08 11:36:15 -0300

    corrigindo erros

--- source_code/CalendarView.swift
+++ source_code/CalendarView.swift
@@ -8,35 +8,6 @@
 // import BackendLib
 // import SwiftUI
 //
-// struct CalendarView: View {
-//    var userName: String = "Julia"
-//    @Binding var date: Date
-//
-//    var monthToPass = Calendar.current.component(.month, from: Date())
-//    var yearToPass = Calendar.current.component(.year, from: Date())
-//
-//    var body: some View {
-////        NavigationView {
-//        ScrollView {
-//            VStack(alignment: .leading) {
-//                Text("Hello, \(userName)")
-//                    .font(.largeTitle)
-//
-//                CalendarComponent(month: monthToPass,
-//                                  year: yearToPass,
-//                                  date: $date)
-//
-//                CycleMomentComponent(phase: .period)
-//            }
-//        }
-////        }
-//        .navigationTitle("Calendar")
-//        .onChange(of: date) {
-//            // mudar a visao de baixo para o dia correto
-//            print("O dia clicado foi \($date.wrappedValue)")
-//        }
-//    }
-// }
 
 import SwiftUI
 
@@ -55,24 +26,9 @@ struct CalendarView: View {
                         .font(.system(.title2))
 
                     HStack(spacing: 30) {
-                        // Calendar
-//                        RoundedRectangle(cornerRadius: 10)
-//                            .frame(width: 460, height: 370)
                         CalendarComponent(month: monthToPass,
                                           year: yearToPass,
                                           date: $date)
-
-//                        CycleMomentComponent(phase: .period)
-
-//                        VStack(alignment: .leading) {
-//                            // Begin cycle
-//                            RoundedRectangle(cornerRadius: 10)
-//                                .frame(width: 142, height: 78)
-//
-//                            // Current cycle phase
-//                            RoundedRectangle(cornerRadius: 10)
-//                                .frame(width: 317, height: 281)
-//                        }
                     }
                     .padding(.bottom, 32)
 
@@ -85,21 +41,6 @@ struct CalendarView: View {
                         .font(.system(.title2))
 
                     CycleMomentComponent(context: modelContext)
-//                    HStack {
-//                        // Calendar
-//                        RoundedRectangle(cornerRadius: 10)
-//                            .frame(width: 460, height: 370)
-//
-//                        VStack(alignment: .leading) {
-//                            // Begin cycle
-//                            RoundedRectangle(cornerRadius: 10)
-//                                .frame(width: 142, height: 78)
-//
-//                            // Current cycle phase
-//                            RoundedRectangle(cornerRadius: 10)
-//                                .frame(width: 317, height: 281)
-//                        }
-//                    }
 
                     Spacer()
                 }
